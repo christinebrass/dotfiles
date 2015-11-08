398e8fb00762a8b6ae07f09d3806e4fe6a0cfcf5
Christine Brass <cybrass@gmail.com>
2015-11-08 13:14:25 -0600

    Add common dir aliases

---
+++ zsh/directories.zsh
@@ -0,0 +1,22 @@
+# Changing/making/removing directory
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+alias d='dirs -v | head -10'
+
+alias pu='pushd'
+alias po='popd'
