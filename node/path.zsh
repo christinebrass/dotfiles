c631eb9aa09a6667fc569d911153e3a4635dbee0
Christine Brass <cybrass@gmail.com>
2018-01-23 10:41:16 -0800

    Disable nvm bash completion to prevent errors

--- node/path.zsh
+++ node/path.zsh
@@ -2,4 +2,6 @@ export PATH=~/.npm-global/bin:$PATH
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Omit completion for zsh
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
