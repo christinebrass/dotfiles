8bbd6c3843475c277f3686729a45de1e6b9dcd34
Christine Brass <cybrass@gmail.com>
2018-05-04 09:27:45 -0700

    Put Python in path

--- python/path.zsh
+++ python/path.zsh
@@ -1,3 +1,4 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$PROJECTS
+export PATH=$PATH:$HOME/Library/Python/2.7/bin
 [ -s "$HOME/Library/Python/2.7/bin/virtualenvwrapper.sh" ] && source $HOME/Library/Python/2.7/bin/virtualenvwrapper.sh
