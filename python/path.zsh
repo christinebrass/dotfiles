373b91adb6a2a75961c9a0d1c9745616cabb0ef2
Christine Brass <cybrass@gmail.com>
2018-05-10 08:34:53 -0700

    Fix rvm

--- python/path.zsh
+++ python/path.zsh
@@ -1,7 +1,4 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$PROJECTS
-#if [[ ! "$(command -v node)" =~ node* ]]
-#then
-#  nvm use default
-#fi
-#[ -s "$HOME/Library/Python/2.7/bin/virtualenvwrapper.sh" ] && source $HOME/Library/Python/2.7/bin/virtualenvwrapper.sh
+export PATH=$PATH:$HOME/Library/Python/2.7/bin
+[ -s "$HOME/Library/Python/2.7/bin/virtualenvwrapper.sh" ] && source $HOME/Library/Python/2.7/bin/virtualenvwrapper.sh
