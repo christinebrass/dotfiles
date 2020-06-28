a7c1fd6b1d9fae71fa29d5bcc3ae3da843a91206
Christine Brass <cybrass@gmail.com>
2020-06-27 20:59:46 -0700

    Fix user directory reference

--- android/path.zsh
+++ android/path.zsh
@@ -3,5 +3,5 @@ export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="/Users/cbrass/.sdkman"
-[[ -s "/Users/cbrass/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/cbrass/.sdkman/bin/sdkman-init.sh"
+export SDKMAN_DIR="${HOME}/.sdkman"
+[[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
