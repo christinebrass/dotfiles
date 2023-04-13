cde3332e6b503b9046b2dda5d705aaae361b2435
Christine Brass <cbrass@truecar.com>
2023-04-13 11:17:39 -0400

    Fix brew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,10 +9,8 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-
   # Install the correct homebrew for each OS type
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
