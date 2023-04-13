9ccbdd5c458d5c229dce30cc151f2a37e8a524dc
Christine Brass <cbrass@truecar.com>
2023-04-13 11:36:08 -0400

    Move nvm post-install scripts to node folder.

---
+++ node/completion.zsh
@@ -0,0 +1,5 @@
+#
+# The nvm install script places this in .zshrc but keep
+# it here for organization.
+#
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
---
+++ node/path.zsh
@@ -0,0 +1,7 @@
+#
+# Add nvm to the path using nvm's script.
+#
+# The nvm install script places this in .zshrc but keep
+# it here for organization.
+#
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
