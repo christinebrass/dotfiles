99210d0b4e788c68aedc68b2a43e0c03ddfbcd11
Christine Brass <cbrass@truecar.com>
2023-04-13 07:52:16 -0400

    No more android dev

--- android/path.zsh
+++
@@ -1,7 +0,0 @@
-export ANDROID_HOME=/usr/local/opt/android-sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
-
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="${HOME}/.sdkman"
-[[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
