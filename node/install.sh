1be2657baf96602cb3cb423241fcabb8f954a93e
Christine Brass <cbrass@truecar.com>
2023-05-12 17:49:14 -0400

    Minor change for testing the review bot

--- node/install.sh
+++ node/install.sh
@@ -3,7 +3,7 @@
 # Install nvm if it doesn't exist
 if [[ ! "$(command -v nvm)" =~ nvm* ]]
 then
-  echo "  Installing nvm for you."
+  echo "  Installing nvm for you!"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 fi
 
